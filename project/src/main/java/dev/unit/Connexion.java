80a311b2e3c02783539dbabe507695a70596acac
christinaRas <rasu.christina@gmail.com>
2025-12-02 17:47:22 +0400

    Add connection to database

---
+++ project/src/main/java/dev/unit/Connexion.java
@@ -0,0 +1,14 @@
+package dev.unit;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/moustass";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
