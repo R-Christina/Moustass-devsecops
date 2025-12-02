dea4a5b18a7724c74ee9b93101b908f98a0f772a
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2025-12-03 02:21:51 +0400

    correction de syntax '{' de la class Connexion

--- project/src/main/java/dev/unit/Connexion.java
+++ project/src/main/java/dev/unit/Connexion.java
@@ -3,12 +3,14 @@ package dev.unit;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class Connexion {
+public class Connexion 
+{
     private static final String URL = "jdbc:mysql://localhost:3306/moustass";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
-    public static Connection getConnection() throws Exception {
+    public static Connection getConnection() throws Exception 
+    {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
