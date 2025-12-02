0bf2beffa77ce042542783eff47042efd96cbf17
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2025-12-03 03:41:22 +0400

    ajout de fonction hash sha256 pour mdp

---
+++ project/src/main/java/dev/back/hash/Hash256.java
@@ -0,0 +1,31 @@
+package dev.back.hash;
+
+import java.security.MessageDigest;
+
+public class Hash256 
+{
+
+    // Hash SHA-256 mdp en hexadécimal
+    public static String hashPassword(String password) 
+    {
+        try 
+        {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hashBytes = digest.digest(password.getBytes("UTF-8"));
+
+            StringBuilder hex = new StringBuilder();
+            for (byte b : hashBytes) {
+                String hexPart = Integer.toHexString(0xff & b);
+                if (hexPart.length() == 1) hex.append('0');
+                hex.append(hexPart);
+            }
+
+            return hex.toString();
+
+        } catch (Exception e) 
+        {
+            throw new RuntimeException("Erreur hash SHA-256 : " + e.getMessage());
+        }
+    }
+}
