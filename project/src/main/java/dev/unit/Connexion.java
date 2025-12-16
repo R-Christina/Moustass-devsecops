fd842cb2cef13d488c1fe03db1ea5b9b73bcc908
christinaRas <rasu.christina@gmail.com>
2025-12-16 16:00:26 +0400

    Modify password from connexion to database

--- project/src/main/java/dev/unit/Connexion.java
+++ project/src/main/java/dev/unit/Connexion.java
@@ -8,12 +8,13 @@ public class Connexion {
 
     private static final String URL = "jdbc:mysql://localhost:3306/moustass";
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws Exception 
     {
         try 
         {
+            System.out.println("DB_PASSWORD = " + System.getenv("DB_PASSWORD"));
             return DriverManager.getConnection(URL, USER, PASSWORD);
 
         } catch (SQLException e) 
